95c65f01c8e59c7d3bbd8b6c29e89940b8bb2de9
absognety <vikasch.1994@gmail.com>
2024-01-25 09:53:25 +0530

    Maximum Number of Palindromic Sequences

---
+++ CodingChalleges/CountofMaxPalindromes.py
@@ -0,0 +1,27 @@
+def isPalindrome(S:str,i:int,j:int) -> int:
+    if i > j:
+        return 1
+    if matrix[i][j] != -1:
+        return matrix[i][j]
+    if S[i] != S[j]:
+        matrix[i][j] = 0
+        return matrix[i][j]
+    matrix[i][j] = isPalindrome(S,i + 1,j - 1)
+    return matrix[i][j]
+
+def numMaxPalindromicSequences(S:str) -> int:
+    n = len(S)
+    count = 0
+    for i in range(n):
+        for j in range(i+1,n):
+            if isPalindrome(S,i,j) != 0:
+                count += 1
+    return count
+    
+if __name__ == '__main__':
+    T = int(input())
+    for tc in range(T):
+        S = input().strip()
+        matrix = [[-1 for i in range(1001)] 
+                        for j in range(1001)]
+        print (numMaxPalindromicSequences(S))
